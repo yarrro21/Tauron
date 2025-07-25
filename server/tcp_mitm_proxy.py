2125bf82f27caf38b308d941c48ebdf243f59e7e
jaroslaw.kurowski <jkl21@go2.pl>
2025-07-25 20:24:41 +0400

    fix

--- server/tcp_mitm_proxy.py
+++ server/tcp_mitm_proxy.py
@@ -74,7 +74,7 @@ def forward(src, dst, direction, print=False):
                 parse_modbus_rtu(data)
             print_hex(direction, data)
             if direction == "C→S":
-                print(f'Data length=', len(data))
+                print(f'Data length=', len('a1111'))
             dst.sendall(data)
     except Exception as e:
         print(f"[!] Error: {e}")
